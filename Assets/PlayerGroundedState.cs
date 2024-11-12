fe2efccc47e9f7340fc6ef4288afdbc2f09a1011
HHUsudra <1062639016@qq.com>
2024-11-12 20:24:16 +0800

    课程2-4（Jump with State Machine）内容

--- Assets/PlayerGroundedState.cs
+++ Assets/PlayerGroundedState.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerIdleState : PlayerGroundedState
+public class PlayerGroundedState : PlayerState
 {
-    public PlayerIdleState(Player _player, PlayerStateMachine _stateMachine, string _animBoolName) : base(_player, _stateMachine, _animBoolName)
+    public PlayerGroundedState(Player _player, PlayerStateMachine _stateMachine, string _animBoolName) : base(_player, _stateMachine, _animBoolName)
     {
+
     }
+
     public override void Enter()
     {
         base.Enter();
@@ -21,9 +23,9 @@ public class PlayerIdleState : PlayerGroundedState
     {
         base.Update();
 
-        if(xInput != 0)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            stateMachine.ChangeState(player.moveState);
+            stateMachine.ChangeState(player.jumpState);
         }
     }
 }
